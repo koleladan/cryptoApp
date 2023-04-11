8f4f7dc72a0f3930aa9ab17b12de3fd5a516bd49
koleladan <isaiahodan@gmail.com>
2023-04-11 12:35:12 +0300

    repository, usecases and di done

---
+++ app/src/main/java/com/compose/cryptoapp/crypto/domain/usecases/GetCoinInfUseCase.kt
@@ -0,0 +1,28 @@
+package com.compose.cryptoapp.crypto.domain.usecases
+
+import com.compose.cryptoapp.core.Resource
+import com.compose.cryptoapp.crypto.data.remote.dto.toCoinInfo
+import com.compose.cryptoapp.crypto.domain.model.CoinInfo
+import com.compose.cryptoapp.crypto.domain.repositoryInterface.CoinsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class GetCoinInfUseCase @Inject constructor(
+    private val repository: CoinsRepository
+) {
+    operator fun invoke(id:String): Flow<Resource<CoinInfo>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coinInfo = repository.GetCoinInfo(id).toCoinInfo()
+            emit(Resource.Success(coinInfo))
+
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "Oops! An error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach the server. Check your connection"))
+        }
+    }
+}
